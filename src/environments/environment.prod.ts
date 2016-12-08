b6de4f8b1b243dfc7d916019129cd20b09e8e8d0
Yury Kozyrev <yury.kozyrev@home24.de>
2016-12-08 17:24:53 +0100

    rm

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  MAIL_KEY: "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",
-  DATABASE_KEY:"AIzaSyAr6GZ98Bhjcr7iiCovz1a8WhAgIl39UZk"
-};
